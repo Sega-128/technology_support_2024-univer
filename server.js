9047abec5ea7b0c2700faf708cd84f70901e8a5e
Євген Белей <zenabelej@gmail.com>
2024-03-21 21:37:58 +0200

    Капец тут кавичок

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-const { PОRT, URL_CLIENT } = require("./config");
-const loggerHTTP = require("./utils/logger.utils");
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { PORT, URL_CLIENT } = require('./config');
+const loggerHTTP = require('./utils/logger.utils');
+
 const app = express();
 const corsOptions = {
  origin: URL_CLIENT,
  credentials: true,
 };
+
 app.use(loggerHTTP);
 app.use(cors(corsOptions));
-app.use("/", require("./routes/index"));
+
+app.use('/', require('./routes/index'));
+
 app.listen(PORT, () => {
- loggerHTTP.logger.info("Start server port: ${PORT}");
+ loggerHTTP.info(`Start server port: ${PORT}`);
 });
